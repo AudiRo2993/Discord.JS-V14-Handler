7c2b75360df8168911daef67ea275a85df29dbfc
AudiRo <118811227+AudiRo7652@users.noreply.github.com>
2022-12-30 17:43:13 +0200

    Update bot.js

--- Structures/bot.js
+++ Structures/bot.js
@@ -47,9 +47,7 @@ class BOT extends Client {
 
   async init(token) {
     await loadEvents(this);
-    if(!BotToken) return console.log(chalk.bold.yellowBright(
-      `[this] -  Please add a Discord Token.  -  [file]: ".env"`
-    ))
+    
     await this.login(BotToken).then(() => {
       console.log(
         chalk.bold.yellowBright(
